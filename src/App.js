472ff8769d136c78c1f7d937a96536a81c150302
amitdwivedi12 <dwivedi.amit154@gmail.com>
2024-12-28 11:51:10 +0530

    ui add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './layout/layout';
+import Header from './shared/header/header';
+import Footer from './shared/footer/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+      <Header />
+      <Layout />
+        <Routes>
+          <Route></Route>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
